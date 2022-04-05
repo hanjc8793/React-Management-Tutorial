fcc6e7a69a71b56232570c5fc13f08bc7f91f86c
hanjc8793 <hanjc8793@gmail.com>
2022-04-05 15:36:14 +0900

    customer_v1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/1',
+    'name' : '한재철',
+    'birthday' : '780625',
+    'gender' : '남자',
+    'job' : '대학생'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : '한준탁',
+    'birthday' : '1111111',
+    'gender' : '남자',
+    'job' : '초등5'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3',
+    'name' : '한준서',
+    'birthday' : '222222',
+    'gender' : '여자',
+    'job' : '초등2'
+  }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" ></img>
-        <h2>Let's develop management system!</h2>
-        123
+      <div>
+        {
+          customers.map(c => {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+                >
+                </Customer>
+            )
+          })
+        }
     </div>
   );
 }
